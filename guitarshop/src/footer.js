62a60f4e8dc92682113ce1fd9b61840cd5f7a59e
Bleron <blero_rraci@hotmail.com>
2025-08-06 19:29:45 +0200

    More components

---
+++ guitarshop/src/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 GuitarShop. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
